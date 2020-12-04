42151bdc9b8f93321d558e32521e7ffbb1445a0a
Trevor Stenson <stenson124@gmail.com>
2020-12-04 00:50:00 -0500

    test consolidation

--- test/aoc_elixir_test.exs
+++ test/aoc_elixir_test.exs
@@ -4,25 +4,16 @@ defmodule AocElixirTest do
 
   test "day 1 expense report" do
     assert 870331 == Day1.find_two_entries("./test/inputs/day1.txt")
-  end
-
-  test "day 1 triple report" do
     assert 283025088 == Day1.find_three_entries("./test/inputs/day1.txt")
   end
 
   test "day 2 valid passwords" do
     assert 564 == Day2.valid_passwords("./test/inputs/day2.txt")
-  end
-
-  test "day 2 part two" do
     assert 325 == Day2.part_two("./test/inputs/day2.txt")
   end
 
   test "day 3" do
     assert 274 == Day3.count_trees("./test/inputs/day3.txt")
-  end
-
-  test "day 3 part two" do
     assert 6050183040 == Day3.part_two_all_slopes("./test/inputs/day3.txt")
   end
 end
